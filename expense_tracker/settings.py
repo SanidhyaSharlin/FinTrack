ca20b17fab9e56460e81df085744de1e95743192
SanidhyaSharlin <sanidhyasharlin@gmail.com>
2026-03-24 15:09:08 +0530

    Configure settings and requirements.txt for Render deployment

--- expense_tracker/settings.py
+++ expense_tracker/settings.py
@@ -19,13 +19,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2fsry#7l*n6508ngr-c*l=42juex^dr794cxhvghr^s#w0jd&h'
+import os
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-2fsry#7l*n6508ngr-c*l=42juex^dr794cxhvghr^s#w0jd&h')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost,*').split(',')
 
 
 # Application definition
@@ -42,6 +41,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -117,6 +117,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Authentication Settings
 LOGIN_URL = 'login'
